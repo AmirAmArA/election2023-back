798e99189a0fbf497cf51ee299b4be011fe7254d
AmirAmArA <39839322+AmirAmArA@users.noreply.github.com>
2023-08-28 13:39:22 +0300

    Update db.js

--- db.js
+++ db.js
@@ -1,6 +1,14 @@
 const mysql = require("mysql2");
 const fs = require('fs');
 
+console.log('Config:', {
+  host: process.env.host,
+  user: process.env.username,
+  password: process.env.password,
+  database: process.env.database,
+  port: process.env.port
+});
+
 const db = mysql.createPool({
   host: process.env.host,
   user: process.env.username,
@@ -9,9 +17,9 @@ const db = mysql.createPool({
   port: process.env.port,
   charset: "utf8mb4",
   waitForConnections: true,
-connectionLimit: 10,
-queueLimit: 0,
-   ssl: {
+  connectionLimit: 10,
+  queueLimit: 0,
+  ssl: {
     ca: fs.readFileSync(__dirname + '/ca-certificate.crt') // Adjust the path accordingly
   }
 });
