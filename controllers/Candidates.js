65ad6468ec2ddccc43260f1b919f68064519f546
AmirAmArA <39839322+AmirAmArA@users.noreply.github.com>
2023-08-28 11:15:14 +0300

    Update Candidates.js

--- controllers/Candidates.js
+++ controllers/Candidates.js
@@ -19,24 +19,13 @@ router.get("/:city", (req, res) => {
   });
 });
 
-app.get('/data', async (req, res) => {
-  try {
-    // Get a connection from the po
-    const connection = await pool.getConnection();
+app.get('/test', async function(req, res){
+const promise= db.promise()
+var sql = "SELECT * FROM candidates"
+const [rows,field] = awiat promise.execute(sql)
 
-    // Execute a query
-    const [rows, fields] = await connection.query('SELECT * FROM candidates');
-
-    // Release the connection back to the pool
-    connection.release();
-
-    // Send the query results as a JSON response
-    res.json(rows);
-  } catch (error) {
-    console.error('Error querying the database:', error);
-    res.status(500).json({ error: 'Internal server error' });
-  }
-});
+res.send(rows)
+ });
 
 router.get("/", (req, res) => {
   const test = "hello test";
