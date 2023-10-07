cf5df3033e9c513a5a8dc58aa0548c0d8ad725f6
AmirAmArA <39839322+AmirAmArA@users.noreply.github.com>
2023-10-07 20:27:09 +0300

    Update historydata.js

--- controllers/historydata.js
+++ controllers/historydata.js
@@ -24,7 +24,7 @@ router.post('/by-date-and-city', (req, res) => {
 });
 
 
-cron.schedule('25 8 * * *', () => {
+cron.schedule('28 20 * * *', () => {
   const insertQuery = 'INSERT INTO historydata (candidatesid, candidatesname, candidatesvotes, datedata, candidatecity) VALUES (?, ?, ?, ?, ?)';
   const q = "SELECT * FROM candidates";
 
