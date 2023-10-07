14fa25c2b8981275a75f5bbdccd347858d8f1204
AmirAmArA <39839322+AmirAmArA@users.noreply.github.com>
2023-10-07 19:44:53 +0300

    Update Candidates.js

--- controllers/Candidates.js
+++ controllers/Candidates.js
@@ -86,7 +86,7 @@ router.put("/:id", (req, res) => {
   //Queries
   const checkQuery = 'SELECT * FROM votes WHERE userAgent = ? AND clientip = ?';
   const updateQuery = "UPDATE candidates SET votes = votes + 1 WHERE id = ?";
-  const userAgentQuery = "INSERT INTO votes ('candidate_id', 'userAgent', 'clientip', 'isvote') VALUES (?,?,?,?)";
+  const userAgentQuery = "INSERT INTO votes (`candidate_id`, `userAgent`, `clientip`, `isvote`) VALUES (?,?,?,?)";
   console.log('user agent:',userAgentQuery)
   db.query(checkQuery, [userAgent, clientIP], (err, results) => {
     if (err) {
